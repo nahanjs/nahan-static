8cb36f6b4e77ebe9565d2f53d1d72f537496ea52
LabMemNo003 <LabMemNo003@gmail.com>
2020-07-13 16:07:26 +0800

    Implement basic functions

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fse = require('fs-extra');
+const path = require('path');
+
+module.exports = Static;
+
+function Static(root) {
+
+    return async (ctx, next) => {
+        if (ctx.path === undefined)
+            ctx.path = ctx.req.url.split('?')[0];
+
+        if (ctx.req.method !== 'GET') {
+            await next();
+        } else {
+            const target = path.join(root, ctx.path);
+            if (! await fse.pathExists(target)) {
+                await next();
+            } else {
+                const data = await fse.readFile(target);
+                ctx.res.write(data);
+            }
+        }
+    };
+}
